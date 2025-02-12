f97083a69b6d2e534cfce66b895cb664b62ca235
zhumengyu <zhumengyu313@163.com>
2025-02-12 10:45:16 +0800

    部署pages白屏提交

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/leaflet-demo-vue3/',
   plugins: [
     vue(),
     vueDevTools(),
